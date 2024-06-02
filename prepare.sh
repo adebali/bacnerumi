a12810ad2da9f609369ad65e44a093c09c90661b
Ogun Adebali <oadebali@gmail.com>
2024-06-02 18:52:04 +0300

    scripts added

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,40 @@
-cat CPSA3_CCAACA_S1_*_R1_001.fastq.gz > CPSA3.fastq.gz && gunzip CPSA3.fastq.gz
-cat CPSMA3_CTAGCT_S2_*_R1_001.fastq.gz > CPSMA3.fastq.gz && gunzip CPSMA3.fastq.gz
-cat CPSUvrD_CGGAAT_S3_*_R1_001.fastq.gz > CPSUvrD.fastq.gz && gunzip CPSUvrD.fastq.gz
-cat CPSWT_CATTTT_S4_*_R1_001.fastq.gz > CPSWT.fastq.gz && gunzip CPSWT.fastq.gz
-cat SmegMfdUMICPDR2_CTCAGA_S5_*_R1_001.fastq.gz > SmegMfdUMICPDR2.fastq.gz && gunzip SmegMfdUMICPDR2.fastq.gz
-cat SmegUvrDMfdUMIR1_TACAGC_S6_*_R1_001.fastq.gz > SmegUvrDMfdUMIR1.fastq.gz && gunzip SmegUvrDMfdUMIR1.fastq.gz
-cat SmegUvrDMfdUMIR2_TATAAT_S7_*_R1_001.fastq.gz > SmegUvrDMfdUMIR2.fastq.gz && gunzip SmegUvrDMfdUMIR2.fastq.gz
-cat SmegUvrDUMICPDR1_GACGAC_S8_*_R1_001.fastq.gz > SmegUvrDUMICPDR1.fastq.gz && gunzip SmegUvrDUMICPDR1.fastq.gz
-cat SmegUvrDUMICPDR2_TAATCG_S9_*_R1_001.fastq.gz > SmegUvrDUMICPDR2.fastq.gz && gunzip SmegUvrDUMICPDR2.fastq.gz
-cat SmegWTUMICPDR2_CTATAC_S10_*_R1_001.fastq.gz > SmegWTUMICPDR2.fastq.gz && gunzip SmegWTUMICPDR2.fastq.gz
+# cat CPSA3_CCAACA_S1_*_R1_001.fastq.gz > CPSA3.fastq.gz && gunzip CPSA3.fastq.gz
+# cat CPSMA3_CTAGCT_S2_*_R1_001.fastq.gz > CPSMA3.fastq.gz && gunzip CPSMA3.fastq.gz
+# cat CPSUvrD_CGGAAT_S3_*_R1_001.fastq.gz > CPSUvrD.fastq.gz && gunzip CPSUvrD.fastq.gz
+# cat CPSWT_CATTTT_S4_*_R1_001.fastq.gz > CPSWT.fastq.gz && gunzip CPSWT.fastq.gz
+# cat SmegMfdUMICPDR2_CTCAGA_S5_*_R1_001.fastq.gz > SmegMfdUMICPDR2.fastq.gz && gunzip SmegMfdUMICPDR2.fastq.gz
+# cat SmegUvrDMfdUMIR1_TACAGC_S6_*_R1_001.fastq.gz > SmegUvrDMfdUMIR1.fastq.gz && gunzip SmegUvrDMfdUMIR1.fastq.gz
+# cat SmegUvrDMfdUMIR2_TATAAT_S7_*_R1_001.fastq.gz > SmegUvrDMfdUMIR2.fastq.gz && gunzip SmegUvrDMfdUMIR2.fastq.gz
+# cat SmegUvrDUMICPDR1_GACGAC_S8_*_R1_001.fastq.gz > SmegUvrDUMICPDR1.fastq.gz && gunzip SmegUvrDUMICPDR1.fastq.gz
+# cat SmegUvrDUMICPDR2_TAATCG_S9_*_R1_001.fastq.gz > SmegUvrDUMICPDR2.fastq.gz && gunzip SmegUvrDUMICPDR2.fastq.gz
+# cat SmegWTUMICPDR2_CTATAC_S10_*_R1_001.fastq.gz > SmegWTUMICPDR2.fastq.gz && gunzip SmegWTUMICPDR2.fastq.gz
+
+
+
+# May 8th 2024
+
+# cat resources/samples/240424_UNC52-VH01297_139_AACHLWGHV/uvrD0_GTGAATAT_S1_*_R1_001.fastq.gz >resources/samples/uvrD0_GTGAATAT.fastq.gz && gunzip resources/samples/uvrD0_GTGAATAT.fastq.gz
+# cat resources/samples/240424_UNC52-VH01297_139_AACHLWGHV/uvrD15_TGCGAGAC_S2_*_R1_001.fastq.gz >resources/samples/uvrD15_TGCGAGAC.fastq.gz && gunzip resources/samples/uvrD15_TGCGAGAC.fastq.gz
+# cat resources/samples/240424_UNC52-VH01297_139_AACHLWGHV/uvrD2_ACAGGCGC_S3_*_R1_001.fastq.gz >resources/samples/uvrD2_ACAGGCGC.fastq.gz && gunzip resources/samples/uvrD2_ACAGGCGC.fastq.gz
+# cat resources/samples/240424_UNC52-VH01297_139_AACHLWGHV/uvrD5_CATAGAGT_S4_*_R1_001.fastq.gz >resources/samples/uvrD5_CATAGAGT.fastq.gz && gunzip resources/samples/uvrD5_CATAGAGT.fastq.gz
+# cat resources/samples/240424_UNC52-VH01297_139_AACHLWGHV/uvrDM0_TCTCTACT_S5_*_R1_001.fastq.gz >resources/samples/uvrDM0_TCTCTACT.fastq.gz && gunzip resources/samples/uvrDM0_TCTCTACT.fastq.gz
+# cat resources/samples/240424_UNC52-VH01297_139_AACHLWGHV/uvrDM15_TTGGACTC_S6_*_R1_001.fastq.gz >resources/samples/uvrDM15_TTGGACTC.fastq.gz && gunzip resources/samples/uvrDM15_TTGGACTC.fastq.gz
+# cat resources/samples/240424_UNC52-VH01297_139_AACHLWGHV/uvrDM2_CTCTCGTC_S7_*_R1_001.fastq.gz >resources/samples/uvrDM2_CTCTCGTC.fastq.gz && gunzip resources/samples/uvrDM2_CTCTCGTC.fastq.gz
+# cat resources/samples/240424_UNC52-VH01297_139_AACHLWGHV/uvrDM5_CCAAGTCT_S8_*_R1_001.fastq.gz >resources/samples/uvrDM5_CCAAGTCT.fastq.gz && gunzip resources/samples/uvrDM5_CCAAGTCT.fastq.gz
+
+
+# May 27th 2024
+
+
+
+
+cat resources/samples/uvrD_rep2/mfd1mA_CTTATCGG_S1_*_R1_001.fastq.gz > resources/samples/mfd1mA_CTTATCGG_rep2.fastq.gz && gunzip resources/samples/mfd1mA_CTTATCGG_rep2.fastq.gz
+cat resources/samples/uvrD_rep2/mfd1mB_TCCGCTAA_S2_*_R1_001.fastq.gz > resources/samples/mfd1mB_TCCGCTAA_rep2.fastq.gz && gunzip resources/samples/mfd1mB_TCCGCTAA_rep2.fastq.gz
+cat resources/samples/uvrD_rep2/uvrD0_GTGAATAT_S5_*_R1_001.fastq.gz > resources/samples/uvrD0_GTGAATAT_rep2.fastq.gz && gunzip resources/samples/uvrD0_GTGAATAT_rep2.fastq.gz
+cat resources/samples/uvrD_rep2/uvrD15_TGCGAGAC_S6_*_R1_001.fastq.gz > resources/samples/uvrD15_TGCGAGAC_rep2.fastq.gz && gunzip resources/samples/uvrD15_TGCGAGAC_rep2.fastq.gz
+cat resources/samples/uvrD_rep2/uvrD2_ACAGGCGC_S7_*_R1_001.fastq.gz > resources/samples/uvrD2_ACAGGCGC_rep2.fastq.gz && gunzip resources/samples/uvrD2_ACAGGCGC_rep2.fastq.gz
+cat resources/samples/uvrD_rep2/uvrD5_CATAGAGT_S8_*_R1_001.fastq.gz > resources/samples/uvrD5_CATAGAGT_rep2.fastq.gz && gunzip resources/samples/uvrD5_CATAGAGT_rep2.fastq.gz
+cat resources/samples/uvrD_rep2/uvrDM0_TCTCTACT_S9_*_R1_001.fastq.gz > resources/samples/uvrDM0_TCTCTACT_rep2.fastq.gz && gunzip resources/samples/uvrDM0_TCTCTACT_rep2.fastq.gz
+cat resources/samples/uvrD_rep2/uvrDM15_GCAATGCA_S10_*_R1_001.fastq.gz > resources/samples/uvrDM15_GCAATGCA_rep2.fastq.gz && gunzip resources/samples/uvrDM15_GCAATGCA_rep2.fastq.gz
+cat resources/samples/uvrD_rep2/uvrDM2_CTCTCGTC_S11_*_R1_001.fastq.gz > resources/samples/uvrDM2_CTCTCGTC_rep2.fastq.gz && gunzip resources/samples/uvrDM2_CTCTCGTC_rep2.fastq.gz
+cat resources/samples/uvrD_rep2/uvrDM5_ATGGCATG_S12_*_R1_001.fastq.gz > resources/samples/uvrDM5_ATGGCATG_rep2.fastq.gz && gunzip resources/samples/uvrDM5_ATGGCATG_rep2.fastq.gz
